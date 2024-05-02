e8eb90b08d60cd5e15d8d75f425f87be5fcacf70
Dennis (s235074) <eren@hotmail.dk>
2024-05-02 12:02:36 +0200

    Added javadoc

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Gear.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Gear.java
@@ -3,6 +3,15 @@ package dk.dtu.compute.se.pisd.roborally.model;
 import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
 import dk.dtu.compute.se.pisd.roborally.controller.GameController;
 
+
+
+/**
+ * @Author s235074
+ * This class represents the Gear object in the game Robo Rally.
+ * The Gear object is a FieldAction and has a direction.
+ *
+ */
+
 public class Gear extends FieldAction {
     public Direction direction;
 
@@ -15,6 +24,14 @@ public class Gear extends FieldAction {
     public Gear(Direction direction) {
         this.direction = direction;
     }
+
+    /**
+     * @Author s235074
+     * This method is used to turn the player in the direction of the gear.
+     * @param gC GameController
+     * @param space Space
+     * @return boolean
+     */
 
     @Override
     public boolean doAction(GameController gC, Space space) {
--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Space.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Space.java
@@ -88,6 +88,14 @@ public class Space extends Subject {
         return walls;
     }
 
+
+    /**
+     * @Author s235074
+     * Add a wall in the given direction to this space.
+     *
+     * @param heading the direction of the wall to be added
+     */
+
     public void addWall (Heading heading){
         if(!walls.contains(heading)){
             walls.add(heading);
